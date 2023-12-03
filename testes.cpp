cfc5722948df5e29829f6b099ec8e9f38a973105
gustavofcunha <gustavocunha182020@gmail.com>
2023-12-03 15:20:10 -0300

    adpatação testes

--- testes.cpp
+++ testes.cpp
@@ -32,7 +32,7 @@ TEST(BancoTest, TransferirComSaldoSuficiente) {
     Banco banco;
     banco.abrirConta();
     banco.abrirConta();
-    auto& contas = banco.getContas();
+    auto contas = banco.getContas();
     contas[0].depositar(100.0, true); 
     banco.transferir();
     EXPECT_EQ(contas[0].consultarSaldo(), 0.0);
@@ -43,7 +43,7 @@ TEST(BancoTest, TransferirComSaldoInsuficiente) {
     Banco banco;
     banco.abrirConta();
     banco.abrirConta();
-    auto& contas = banco.getContas();
+    auto contas = banco.getContas();
     banco.transferir(); // tenta transferir sem saldo suficiente
     EXPECT_EQ(contas[0].consultarSaldo(), 0.0);
     EXPECT_EQ(contas[1].consultarSaldo(), 0.0);
@@ -52,7 +52,7 @@ TEST(BancoTest, TransferirComSaldoInsuficiente) {
 TEST(BancoTest, EncerrarContaExistente) {
     Banco banco;
     banco.abrirConta();
-    auto& contas = banco.getContas();
+    auto contas = banco.getContas();
     banco.encerrarConta(); // encerra a conta existente
     EXPECT_EQ(contas.size(), 0);
 }
@@ -60,7 +60,7 @@ TEST(BancoTest, EncerrarContaExistente) {
 TEST(BancoTest, EncerrarContaInexistente) {
     Banco banco;
     banco.abrirConta();
-    auto& contas = banco.getContas();
+    auto contas = banco.getContas();
     banco.encerrarConta(); // tenta encerrar uma conta inexistente
     EXPECT_EQ(contas.size(), 1);
 }
