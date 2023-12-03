1bc6a5872ef187f057b61baf107b2011cc247a71
gustavofcunha <gustavocunha182020@gmail.com>
2023-12-03 14:27:34 -0300

    testes com gtests e novo workflow

--- testes.cpp
+++ testes.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
-#include <assert.h>
+#include <gtest/gtest.h>
 #include "include/banco.h"
 #include "include/conta.h"
 
-void testAbrirConta() {
+TEST(BancoTest, AbrirConta) {
     Banco banco;
     banco.abrirConta();
     const std::vector<Conta>& contas = banco.getContas();
-    assert(!contas.empty());
+    ASSERT_FALSE(contas.empty());
 }
 
-void testDepositar() {
+TEST(BancoTest, Depositar) {
     Banco banco;
     banco.abrirConta();
     const std::vector<Conta>& contas = banco.getContas();
@@ -18,74 +18,12 @@ void testDepositar() {
     double saldoAntes = contas[0].consultarSaldo();
     banco.depositar();
     double saldoDepois = contas[0].consultarSaldo();
-    assert(saldoDepois > saldoAntes);
+    ASSERT_GT(saldoDepois, saldoAntes);
 }
 
-void testSaque() {
-    Banco banco;
-    banco.abrirConta();
-    const std::vector<Conta>& contas = banco.getContas();
-    int numeroConta = contas[0].getNumeroConta();
-    double saldoAntes = contas[0].consultarSaldo();
-    banco.sacar();
-    double saldoDepois = contas[0].consultarSaldo();
-    assert(saldoDepois < saldoAntes);
+// Adicione mais TESTs conforme necessário...
+
+int main(int argc, char** argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
-
-void testTransferencia() {
-    Banco banco;
-    banco.abrirConta();
-    banco.abrirConta();
-    const std::vector<Conta>& contas = banco.getContas();
-    int contaOrigem = contas[0].getNumeroConta();
-    int contaDestino = contas[1].getNumeroConta();
-    double saldoOrigemAntes = contas[0].consultarSaldo();
-    double saldoDestinoAntes = contas[1].consultarSaldo();
-    banco.transferir();
-    double saldoOrigemDepois = contas[0].consultarSaldo();
-    double saldoDestinoDepois = contas[1].consultarSaldo();
-    assert(saldoOrigemDepois < saldoOrigemAntes);
-    assert(saldoDestinoDepois > saldoDestinoAntes);
-}
-
-void testEncerrarConta() {
-    Banco banco;
-    banco.abrirConta();
-    const std::vector<Conta>& contas = banco.getContas();
-    int numeroConta = contas[0].getNumeroConta();
-    banco.encerrarConta();
-    const std::vector<Conta>& contasDepois = banco.getContas();
-    assert(contasDepois.empty());
-}
-
-void testExtratoConta() {
-    Banco banco;
-    banco.abrirConta();
-    const std::vector<Conta>& contas = banco.getContas();
-    int numeroConta = contas[0].getNumeroConta();
-    banco.verExtratoConta();
-}
-
-void testInformacoesConta() {
-    Banco banco;
-    banco.abrirConta();
-    const std::vector<Conta>& contas = banco.getContas();
-    int numeroConta = contas[0].getNumeroConta();
-    banco.verInformacoesConta();
-}
-
-#ifdef RUN_TESTS
-int main() {
-    testAbrirConta();
-    testDepositar();
-    testSaque();
-    testTransferencia();
-    testEncerrarConta();
-    testExtratoConta();
-    testInformacoesConta();
-
-    std::cout << "Todos os testes passaram com sucesso!" << std::endl;
-
-    return 0;
-}
-#endif
