b6d66530c7f7cc9da9cecd855f8a8c425c5f8ea0
gustavofcunha <gustavocunha182020@gmail.com>
2023-12-03 14:57:17 -0300

    correção de erros de referência

--- testes.cpp
+++ testes.cpp
@@ -2,38 +2,32 @@
 #include "include/banco.h"
 #include "include/conta.h"
 
-
 TEST(BancoTest, MaiorNumeroConta) {
-    // Crie um objeto Banco e adicione algumas contas
     Banco banco;
-    banco.abrirConta(); // Adicione uma conta
-    banco.abrirConta(); // Adicione outra conta
+    banco.abrirConta();
+    banco.abrirConta();
 
-    // O maior número de conta deve ser 2, pois iniciamos de 1
-    EXPECT_EQ(banco.maiorNumeroConta(), 2);
+    const Banco& constBanco = banco;  // Create a const reference
+    EXPECT_EQ(constBanco.maiorNumeroConta(), 2);
 }
-
 
 TEST(BancoTest, AbrirConta) {
     Banco banco;
     banco.abrirConta();
 
-    // Verifica se a conta foi adicionada ao vetor de contas
-    EXPECT_EQ(banco.getContas().size(), 1);
+    const Banco& constBanco = banco;  // Create a const reference
+    EXPECT_EQ(constBanco.getContas().size(), 1);
 }
 
 TEST(BancoTest, Depositar) {
     Banco banco;
     banco.abrirConta();
     
-    // Obtém a conta recém-criada
-    Conta& conta = banco.getContas().front();
-    
-    // Deposita um valor na conta
+    const Banco& constBanco = banco;  // Create a const reference
+    const Conta& conta = constBanco.getContas().front();
+
     double valorDeposito = 100.0;
     conta.depositar(valorDeposito, true);
-
-    // Verifica se o saldo da conta é igual ao valor depositado
     EXPECT_EQ(conta.consultarSaldo(), valorDeposito);
 }
 
@@ -41,28 +35,20 @@ TEST(BancoTest, Sacar) {
     Banco banco;
     banco.abrirConta();
     
-    // Obtém a conta recém-criada
-    Conta& conta = banco.getContas().front();
+    const Banco& constBanco = banco;  // Create a const reference
+    const Conta& conta = constBanco.getContas().front();
     
-    // Deposita um valor inicial
     double valorInicial = 200.0;
     conta.depositar(valorInicial, true);
 
-    // Tenta sacar um valor menor que o saldo
     double valorSaque = 100.0;
     EXPECT_TRUE(conta.sacar(valorSaque, true));
-
-    // Verifica se o saldo foi reduzido corretamente
     EXPECT_EQ(conta.consultarSaldo(), valorInicial - valorSaque);
 
-    // Tenta sacar um valor maior que o saldo
     double valorSaqueMaiorQueSaldo = 500.0;
     EXPECT_FALSE(conta.sacar(valorSaqueMaiorQueSaldo, true));
-
-    // O saldo não deve ter sido alterado neste caso
     EXPECT_EQ(conta.consultarSaldo(), valorInicial - valorSaque);
 }
-
 
 int main(int argc, char** argv) {
     testing::InitGoogleTest(&argc, argv);
